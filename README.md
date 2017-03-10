2fff2b923fa534f71e3b26907d94f44718f56b07
Alex Katkov <alexander.katkov@outlook.com>
2017-03-10 17:44:38 +0300

    Apache2 & Virtual hosts provision scripts.

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# BlackBox v0.1.0
+# BlackBox v0.2.0
 Vagrant LAMP stack configuration.
---
+++ scripts/intro.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+#
+# BlackBox intro provision file
+
+echo $'\n' 
+echo "---------------------------------------------"
+echo "Welcome to BlackBox"
+echo "---------------------------------------------"
+echo $'Starting provision ... \n'
+echo $'\n'
