0df634ae36840ad2ece2f8908d96e887f67986ad
Alex K <alexander.katkov@outlook.com>
2017-10-03 00:26:14 +0300

    #2 switched from fpm to lib. modified xdebug config

--- scripts/apache2.sh
+++ scripts/apache2.sh
@@ -27,6 +27,8 @@ else
 
   #	Customization of HTTP request and response headers support
   sudo a2enmod headers
+  # Provides a rule-based rewriting engine to rewrite requested URLs on the fly
+	sudo a2enmod rewrite
   # Restart Apache2 to activate configuration
   sudo service apache2 restart
 fi
