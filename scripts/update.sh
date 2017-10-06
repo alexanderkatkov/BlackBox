ad8c588f446a66039c4108553a79a328fad408f9
Alex K <alexander.katkov@outlook.com>
2017-10-06 21:31:02 +0300

    #20 update provision

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,15 +1,12 @@
 #!/usr/bin/env bash
 #
 # System silent update file
+echo " >>> System & packages update."
 
-echo $'\n'
-echo "---------------------------------------------"
-echo "System & packages silent update."
-echo "---------------------------------------------"
-echo $'\n'
 # Updating & upgrading packages
-
 # Update repository list
 sudo apt-get update -y
-# Upgrade packages with suspending GRUB configuration modal
+# Upgrade packages with suspending GRUB configuration request
 sudo DEBIAN_FRONTEND=noninteractive apt-get -y -o DPkg::options::="--force-confdef" -o DPkg::options::="--force-confold" upgrade
+# Removing unnecessary packages
+sudo apt-get autoremove -y
