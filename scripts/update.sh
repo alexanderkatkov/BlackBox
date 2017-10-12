f9b89e1df39f24fe16768b93039b21bd564dd573
Alex K <alexander.katkov@outlook.com>
2017-10-12 23:09:14 +0300

    #20 global refactor with major fixes

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,11 +1,10 @@
 #!/usr/bin/env bash
 #
-# System silent update file
-echo " >>> System & packages update."
+# Updating repository list & upgrading packages
+echo ">>> System & packages update."
 
-# Updating & upgrading packages
 # Update repository list
-sudo apt-get update -y
+sudo apt-get -y update
 # Upgrade packages with suspending GRUB configuration request
 sudo DEBIAN_FRONTEND=noninteractive apt-get -y -o DPkg::options::="--force-confdef" -o DPkg::options::="--force-confold" upgrade
 # Removing unnecessary packages
